895e8c767c42acd86afa29f2a07b71136efb12ad
Яна Микрюкова <yamikryukova@yandex.ru>
2026-04-25 19:52:19 +0300

    Добавлены модели Order, OrderItem, Contact и ConfirmEmailToken, инициализированы миграции

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django_rest_passwordreset.tokens import get_token_generator
 
 # Статусы, в которых может находиться заказанная корзина
 STATE_CHOICES = (
@@ -209,3 +210,92 @@ class ProductParameter(models.Model):
 
     def __str__(self):
         return f"{self.product_info.product.name} - {self.parameter.name}"
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(
+        "User", verbose_name="Пользователь", related_name="contacts", blank=True, on_delete=models.CASCADE
+    )
+    city = models.CharField(max_length=50, verbose_name="Город")
+    street = models.CharField(max_length=100, verbose_name="Улица")
+    house = models.CharField(max_length=15, verbose_name="Дом", blank=True)
+    structure = models.CharField(max_length=15, verbose_name="Корпус", blank=True)
+    building = models.CharField(max_length=15, verbose_name="Строение", blank=True)
+    apartment = models.CharField(max_length=15, verbose_name="Квартира", blank=True)
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+
+    class Meta:
+        verbose_name = "Контакты пользователя"
+        verbose_name_plural = "Список контактов пользователя"
+
+    def __str__(self):
+        return f"{self.city} {self.street} {self.house}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        "User", verbose_name="Пользователь", related_name="orders", blank=True, on_delete=models.CASCADE
+    )
+    dt = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(verbose_name="Статус", choices=STATE_CHOICES, max_length=15)
+    contact = models.ForeignKey(Contact, verbose_name="Контакт", blank=True, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Список заказов"
+        ordering = ("-dt",)
+
+    def __str__(self):
+        return str(self.dt)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, verbose_name="Заказ", related_name="ordered_items", blank=True, on_delete=models.CASCADE
+    )
+    product_info = models.ForeignKey(
+        ProductInfo,
+        verbose_name="Информация о товаре",
+        related_name="ordered_items",
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+
+    class Meta:
+        verbose_name = "Заказанная позиция"
+        verbose_name_plural = "Список заказанных позиций"
+        constraints = [
+            models.UniqueConstraint(fields=["order", "product_info"], name="unique_order_item"),
+        ]
+
+    def __str__(self):
+        return f"{self.product_info.product.name} ({self.quantity} шт.)"
+
+
+class ConfirmEmailToken(models.Model):
+    user = models.ForeignKey(
+        "User",
+        related_name="confirm_email_tokens",
+        on_delete=models.CASCADE,
+        verbose_name=_("Пользователь, связанный с этим токеном"),
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Когда был сгенерирован этот токен"))
+    key = models.CharField(_("Key"), max_length=64, db_index=True, unique=True)
+
+    class Meta:
+        verbose_name = "Токен подтверждения Email"
+        verbose_name_plural = "Токены подтверждения Email"
+
+    def __str__(self):
+        return f"Токен подтверждения для пользователя {self.user}"
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_key():
+        """Генерирует уникальный токен"""
+        return get_token_generator().generate_token()

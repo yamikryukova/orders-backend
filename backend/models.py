33936974bf3de25279e2b19662bd3606d4a63bb1
Яна Микрюкова <yamikryukova@yandex.ru>
2026-04-25 19:31:41 +0300

    Добавлены модели каталога: Product, ProductInfo, Parameter, ProductParameter

--- backend/models.py
+++ backend/models.py
@@ -134,3 +134,78 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=80, verbose_name="Название")
+    category = models.ForeignKey(
+        Category, verbose_name="Категория", related_name="products", blank=True, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Список товаров"
+        ordering = ("-name",)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductInfo(models.Model):
+    model = models.CharField(max_length=80, verbose_name="Модель", blank=True)
+    external_id = models.PositiveIntegerField(verbose_name="Внешний ИД")
+    product = models.ForeignKey(
+        Product, verbose_name="Товар", related_name="product_infos", blank=True, on_delete=models.CASCADE
+    )
+    shop = models.ForeignKey(
+        Shop, verbose_name="Магазин", related_name="product_infos", blank=True, on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    price = models.PositiveIntegerField(verbose_name="Цена")
+    price_rrc = models.PositiveIntegerField(verbose_name="Рекомендуемая розничная цена")
+
+    class Meta:
+        verbose_name = "Информация о товаре"
+        verbose_name_plural = "Информационный список о товарах"
+        constraints = [
+            models.UniqueConstraint(fields=["product", "shop", "external_id"], name="unique_product_info"),
+        ]
+
+    def __str__(self):
+        return f"{self.product.name} ({self.shop.name})"
+
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=40, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Имя параметра"
+        verbose_name_plural = "Список имен параметров"
+        ordering = ("-name",)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductParameter(models.Model):
+    product_info = models.ForeignKey(
+        ProductInfo,
+        verbose_name="Информация о товаре",
+        related_name="product_parameters",
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    parameter = models.ForeignKey(
+        Parameter, verbose_name="Параметр", related_name="product_parameters", blank=True, on_delete=models.CASCADE
+    )
+    value = models.CharField(verbose_name="Значение", max_length=100)
+
+    class Meta:
+        verbose_name = "Параметр"
+        verbose_name_plural = "Список параметров"
+        constraints = [
+            models.UniqueConstraint(fields=["product_info", "parameter"], name="unique_product_parameter"),
+        ]
+
+    def __str__(self):
+        return f"{self.product_info.product.name} - {self.parameter.name}"
